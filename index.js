99f19c0c81aabd61ed0620f9ae08ec6180a2a1a6
Leonard Raditya <raditya.leonard@gmaiil.com>
2025-12-01 10:32:26 +0700

    First Commit

---
+++ index.js
@@ -0,0 +1,25 @@
+import express from 'express';
+// import bodyParser from 'body-parser';
+
+const app = express();
+const port = 8080;
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.render('index.ejs', {
+    guestName : 'Leonard Raditya',
+    groom : 'Sondra',
+    bride : 'Felita'
+  })
+});
+
+// gausah buat button pake yang move ke page lain
+// app.post('/submit', (req, res) => {
+//     res.render('invite.ejs');
+// });
+
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
